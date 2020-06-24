ac5ebf084b57de87c5349180d69199b25676b369
zhangshyue <zhangshiyuez@gmail.com>
2020-06-24 21:26:35 +0000

    add title to gallery

---
+++ portfolio/src/main/webapp/gallery.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.invite-scroll', { delay: 200, reset: true, desktop: true });
+
+// ScrollReveal().reveal('.headline2', { delay: 200, reset: true, desktop: true });
+// ScrollReveal().reveal('.headline3', { delay: 400, reset: true, desktop: true });
+// ScrollReveal().reveal('.headline4', { delay: 600, reset: true, desktop: true, opacity: 0.1 });
+// ScrollReveal().reveal('.headline4text', { delay: 600, reset: true, desktop: true, opacity: 0.05, duration: 2000 });

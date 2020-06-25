b73b47b0c5bb1494e20149b48892c8f8e16be8a0
zhangshyue <zhangshiyuez@gmail.com>
2020-06-25 15:25:55 +0000

    fix js animation though it's still not working perfectly

--- portfolio/src/main/webapp/gallery.js
+++ portfolio/src/main/webapp/gallery.js
@@ -1,11 +1,7 @@
-ScrollReveal().reveal('.invite-scroll', { delay: 200, desktop: true });
-ScrollReveal().reveal('.france h1', { delay: 200, desktop: true });
-ScrollReveal().reveal('.france figure', { delay: 200, desktop: true });
-ScrollReveal().reveal('.greece h1', { delay: 200, desktop: true });
-ScrollReveal().reveal('.greece figure', { delay: 200, desktop: true });
-ScrollReveal().reveal('.spain h1', { delay: 200, desktop: true });
-ScrollReveal().reveal('.spain figure', { delay: 200, desktop: true });
-// ScrollReveal().reveal('.headline2', { delay: 200, reset: true, desktop: true });
-// ScrollReveal().reveal('.headline3', { delay: 400, reset: true, desktop: true });
-// ScrollReveal().reveal('.headline4', { delay: 600, reset: true, desktop: true, opacity: 0.1 });
-// ScrollReveal().reveal('.headline4text', { delay: 600, reset: true, desktop: true, opacity: 0.05, duration: 2000 });
+ScrollReveal().reveal('.invite-scroll', { origin: 'right', duration: 2000 });
+ScrollReveal().reveal('.france h1', { origin: 'right', duration: 2000 });
+ScrollReveal().reveal('.france figure', { origin: 'right', duration: 2000 });
+ScrollReveal().reveal('.greece h1',{ origin: 'right', duration: 2000 });
+ScrollReveal().reveal('.greece figure', { origin: 'right', duration: 2000 });
+ScrollReveal().reveal('.spain h1', { origin: 'right', duration: 2000 });
+ScrollReveal().reveal('.spain figure', { origin: 'right', duration: 2000 });

1ae7f5cd6f8142d2b789043a5cc1f3a014cc24fa
zhangshyue <zhangshiyuez@gmail.com>
2020-06-25 14:34:38 +0000

    modify style and class name

--- portfolio/src/main/webapp/gallery.js
+++ portfolio/src/main/webapp/gallery.js
@@ -1,6 +1,10 @@
 ScrollReveal().reveal('.invite-scroll', { delay: 200, desktop: true });
 ScrollReveal().reveal('.france h1', { delay: 200, desktop: true });
 ScrollReveal().reveal('.france figure', { delay: 200, desktop: true });
+ScrollReveal().reveal('.greece h1', { delay: 200, desktop: true });
+ScrollReveal().reveal('.greece figure', { delay: 200, desktop: true });
+ScrollReveal().reveal('.spain h1', { delay: 200, desktop: true });
+ScrollReveal().reveal('.spain figure', { delay: 200, desktop: true });
 // ScrollReveal().reveal('.headline2', { delay: 200, reset: true, desktop: true });
 // ScrollReveal().reveal('.headline3', { delay: 400, reset: true, desktop: true });
 // ScrollReveal().reveal('.headline4', { delay: 600, reset: true, desktop: true, opacity: 0.1 });

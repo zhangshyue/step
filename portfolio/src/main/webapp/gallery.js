08d47581057cb2f97c20224e39ee7a6a822983de
zhangshyue <zhangshiyuez@gmail.com>
2020-06-26 22:15:04 +0000

    add css and js animation effect

--- portfolio/src/main/webapp/gallery.js
+++ portfolio/src/main/webapp/gallery.js
@@ -1,4 +1,4 @@
-ScrollReveal().reveal('.invite-scroll', { origin: 'right', duration: 2000 });
+ScrollReveal().reveal('.invite-scroll', { origin: 'right', duration: 4000 });
 ScrollReveal().reveal('.france h1', { origin: 'right', duration: 2000 });
 ScrollReveal().reveal('.france figure', { origin: 'right', duration: 2000 });
 ScrollReveal().reveal('.greece h1',{ origin: 'right', duration: 2000 });

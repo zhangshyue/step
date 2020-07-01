e89d79d11c1660f043ca6193c6c927974a5d1685
zhangshyue <zhangshiyuez@gmail.com>
2020-07-01 16:16:12 +0000

    add url /delete-data to delete contents in datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -99,3 +99,4 @@ public class DataServlet extends HttpServlet {
     }
 }
 
+
---
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -0,0 +1,39 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.sps.data.DataStats;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that delete comments data */
+@WebServlet("/delete-data")
+public class DeleteDataServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Query query = new Query("Entry").addSort("name", SortDirection.ASCENDING );
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery results = datastore.prepare(query);
+        
+        for (Entity entity : results.asIterable()) {
+            Key key = entity.getKey();
+            datastore.delete(key);
+        }
+
+        // Redirect back to the HTML page.
+        response.sendRedirect("/index.html");
+    }
+    
+}

e54a38d6afb8cd1cbbaeb8f4b444204c367779c0
zhangshyue <zhangshiyuez@gmail.com>
2020-07-01 15:39:17 +0000

    let user select max displayed comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,7 +16,8 @@
  * Fetches content from the server and adds it to the DOM.
  */
 function getContentFunctions() {
-    fetch('/data?number=5').then(response => response.text()).then((comments) => {
+    let num = document.querySelector('[name="number-comments"]').value;
+    fetch('/data?number='+num).then(response => response.text()).then((comments) => {
         console.log(comments);
         comments = JSON.parse(comments);
         const statsListElement = document.getElementById('comment-container');

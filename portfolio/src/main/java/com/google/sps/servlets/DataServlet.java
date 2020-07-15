4e5c5d944b99100822b7f3cc44db8aad6707acf5
zhangshyue <zhangshiyuez@gmail.com>
2020-07-15 19:52:08 +0000

    fix blobstore issue

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -153,32 +153,21 @@ public class DataServlet extends HttpServlet {
         List<BlobKey> blobKeys = blobs.get("image");
 
         // User submitted form without selecting a file, so we can't get a URL. (dev server)
-        if (blobKeys == null || blobKeys.isEmpty()) {
-            return null;
-        }
+        // if (blobKeys == null || blobKeys.isEmpty()) {
+        //     return null;
+        // }
 
         // Our form only contains a single file input, so get the first index.
         BlobKey blobKey = blobKeys.get(0);
 
         // User submitted form without selecting a file, so we can't get a URL. (live server)
-        // BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
-        // if (blobInfo.getSize() == 0) {
-        //     blobstoreService.delete(blobKey);
-        //     return null;
-        // }
-
-        // Use ImagesService to get a URL that points to the uploaded file.
-        ImagesService imagesService = ImagesServiceFactory.getImagesService();
-        ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
-
-        // To support running in Google Cloud Shell with AppEngine's devserver, we must use the relative
-        // path to the image, rather than the path returned by imagesService which contains a host.
-        try {
-            URL url = new URL(imagesService.getServingUrl(options));
-            return url.getPath();
-        } catch (MalformedURLException e) {
-            return imagesService.getServingUrl(options);
+        BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+        if (blobInfo.getSize() == 0) {
+            blobstoreService.delete(blobKey);
+            return null;
         }
+        
+        return blobKey.getKeyString();
     }
 }
 
